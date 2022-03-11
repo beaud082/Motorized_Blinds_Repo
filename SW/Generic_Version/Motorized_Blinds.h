5405507ee703066742ce3a3710b985f74442edf7
beaud082 <37131641+beaud082@users.noreply.github.com>
2022-03-10 22:23:27 -0800

    Commit

--- SW/Generic_Version/Motorized_Blinds.h
+++ SW/Generic_Version/Motorized_Blinds.h
@@ -6,9 +6,11 @@
 #define TILT_STEP_MAX 100 //Max-Min = total travelable stepping area (measured in full steps)
 #define TILT_STEP_HOME 50 //A value between the Max and Min that will act as the hardware programmed home location.
 
-#define STEPPER_SLEEP_PIN 9
+#define STEPPER_RST_PIN 2
 #define STEPPER_STEP_PIN 5
 #define STEPPER_DIR_PIN 4
+#define STEPPER_SLEEP_PIN 0
+#define STEPPER_EN_PIN 16
 
 #define CAL_BUTTON_PIN 1
 #define UP_BUTTON_PIN 3
@@ -27,12 +29,25 @@ class Motorized_Blinds : public Component, public CustomAPIDevice {
   void setup() override {
     // This will be called by App.setup()
 	ESP_LOGD("custom", "My setup() called");
+	
+	pinMode(STEPPER_EN_PIN, OUTPUT);
+	digitalWrite(STEPPER_EN_PIN, LOW);//Active low
+	pinMode(STEPPER_RST_PIN, OUTPUT);
+	digitalWrite(STEPPER_RST_PIN, HIGH);//Active low
+	
 	stepper_motor.begin(RPM, STEPS_PER_REVOLUTION);
-	ESP_LOGD("custom", "stepper_motor.begin(*,*) completed");
-	/*
 	stepper_motor.setEnableActiveState(LOW);
 	stepper_motor.disable();
-	*/
+	
+	ESP_LOGD("custom", "Stepper motor setup complete.");
+	
+	//pinMode(CAL_BUTTON_PIN, INPUT);
+	//pinMode(UP_BUTTON_PIN, INPUT);
+	pinMode(DOWN_BUTTON_PIN, INPUT);
+	pinMode(HOME_BUTTON_PIN, INPUT);
+	
+	ESP_LOGD("custom", "Button setup complete.");
+
 	// Declare a service "calibrate_blinds"
     //  - Service will be called "esphome.<NODE_NAME>_calibrate_blinds" in Home Assistant.
     //  - The service has no arguments
@@ -50,12 +65,16 @@ class Motorized_Blinds : public Component, public CustomAPIDevice {
     //  - Service will be called "esphome.<NODE_NAME>_stop_blinds" in Home Assistant.
     //  - The service has no arguments
     //  - The function stop_blinds declared below will attached to the service.
-    register_service(&Motorized_Blinds::stop_blinds, "stop_blinds");	
+    register_service(&Motorized_Blinds::stop_blinds, "stop_blinds");
+	
+	calibrate();	
+	
 	ESP_LOGD("custom", "My setup() completed");
   }
   
   void calibrate(){
 	ESP_LOGD("custom", "calibrate called");
+	stepper_motor.enable();
 	stepper_motor.startMove((int)(-1.05 * ((double) TILT_STEP_MAX)));
 	current_step_tilt = 0;
   }
@@ -90,15 +109,21 @@ class Motorized_Blinds : public Component, public CustomAPIDevice {
   
   void home_blinds(){
 	ESP_LOGD("custom", "home_blinds called");
-	stepper_motor.startMove(TILT_STEP_HOME - current_step_tilt);
-	current_step_tilt = TILT_STEP_HOME;
+	if(stepper_motor.getCurrentState() == BasicStepperDriver::STOPPED) {
+		stepper_motor.enable();
+		stepper_motor.startMove(TILT_STEP_HOME - current_step_tilt);
+		current_step_tilt = TILT_STEP_HOME;
+	} 
+	else {
+		ESP_LOGD("custom", "home_blinds ignored - already moving");
+	}
+	
   }
   
   
   void loop() override {
 	
 	unsigned time_stamp_micros = micros();
-	/*
 	if(time_stamp_micros - next_action_time_micros >= 0){
 		unsigned interval_time_micros = stepper_motor.nextAction();
 		if(interval_time_micros == 0){
@@ -109,7 +134,6 @@ class Motorized_Blinds : public Component, public CustomAPIDevice {
 			next_action_time_micros = time_stamp_micros + interval_time_micros;
 		}
 	}
-	*/
 	if(time_stamp_micros - next_button_time_micros >= 0){
 		next_button_time_micros = 0;
 		
